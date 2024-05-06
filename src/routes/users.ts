576833d578fb5be261a8c0cb1b249a11e2e11e5f
yanaemon <hiroki.shirayanagi@gmail.com>
2024-05-06 18:20:57 +0900

    :recycle: add valiation example

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,14 @@
 import * as express from 'express'
 import { User } from '../models'
+import * as validator from 'validator'
 
 export async function list(req: express.Request, res: express.Response) {
-  const users = await User.find(req.query)
+  const condition: any = {}
+  if (req.query.email) {
+    condition.email = req.query.email
+  }
+
+  const users = await User.find(condition)
   return res.json(users)
 }
 
@@ -15,6 +21,10 @@ export async function show(req: express.Request, res: express.Response) {
 }
 
 export async function create(req: express.Request, res: express.Response) {
+  if (!validator.isEmail(req.body.email)) {
+    return res.status(400).json({ message: 'invalid email' })
+  }
+
   const user = await User.create(req.body)
   return res.json(user)
 }
